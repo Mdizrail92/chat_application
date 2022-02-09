8b2ed3cc82c2f6196017cf0eca4e2e7f7d938780
Israil Alam <52991105+Mdizrail92@users.noreply.github.com>
2022-02-10 01:38:19 +0530

    Hiding project ID

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID='ccf7743e-b2e9-4185-ad05-23ce61d1f1ea';
+const projectID='Your-ChatEngine-projectID';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
